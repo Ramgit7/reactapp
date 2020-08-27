dbe2e3de4f094b8cf6b6b33bb666e2fe4e3aa605
Ramgit7 <rampm789@gmail.com>
2020-08-27 07:08:28 +0530

    Updates

---
+++ precache-manifest.4a4372af8a8f5a081b7583bc67e405fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1138e8514a9217c9c2d6414369935f91",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "516444bbe5ec1f0aa376",
+    "url": "/reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/reactapp/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516444bbe5ec1f0aa376",
+    "url": "/reactapp/static/js/main.b3497dea.chunk.js"
+  },
+  {
+    "revision": "8c1959f69375e3823f96",
+    "url": "/reactapp/static/js/runtime-main.1f569a7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
